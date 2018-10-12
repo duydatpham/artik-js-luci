db7653e27074549d63347ce5a61f3ef2cca24dfe
Duy Dat Pham <duydatpham@gmail.com>
2018-10-12 10:39:10 +0700

    update zigbee

--- src/coap.js
+++ src/coap.js
@@ -1,129 +1,135 @@
 var events = require('events');
 var util = require('util');
-var coap = require('../build/Release/artik-sdk.node').coap;
 
-var Coap = function(coap_config) {
-	events.EventEmitter.call(this);
-	this.coap = new coap(coap_config);
-}
+var coap = require('artik-sdk').coap;
 
-util.inherits(Coap, events.EventEmitter);
+if (!!coap && !!coap.destroy_server) {
+	module.exports = coap
+} else {
 
-module.exports = Coap;
+	var Coap = function (coap_config) {
+		events.EventEmitter.call(this);
+		this.coap = new coap(coap_config);
+	}
 
-Coap.prototype.create_client = function() {
-	return this.coap.create_client();
-}
+	util.inherits(Coap, events.EventEmitter);
 
-Coap.prototype.destroy_client = function() {
-	return this.coap.destroy_client();
-}
+	module.exports = Coap;
 
-Coap.prototype.connect = function() {
-	return this.coap.connect();
-}
+	Coap.prototype.create_client = function () {
+		return this.coap.create_client();
+	}
 
-Coap.prototype.disconnect = function() {
-	return this.coap.disconnect();
-}
+	Coap.prototype.destroy_client = function () {
+		return this.coap.destroy_client();
+	}
 
-Coap.prototype.create_server = function() {
-	return this.coap.create_server();
-}
+	Coap.prototype.connect = function () {
+		return this.coap.connect();
+	}
 
-Coap.prototype.destroy_server = function() {
-	return this.coap.destroy_server();
-}
+	Coap.prototype.disconnect = function () {
+		return this.coap.disconnect();
+	}
 
-Coap.prototype.start_server = function(verify_psk_cb) {
-	return this.coap.start_server(verify_psk_cb);
-}
+	Coap.prototype.create_server = function () {
+		return this.coap.create_server();
+	}
 
-Coap.prototype.stop_server = function() {
-	return this.coap.stop_server();
-}
+	Coap.prototype.destroy_server = function () {
+		return this.coap.destroy_server();
+	}
 
-Coap.prototype.send_message = function(path, coap_msg, send_cb) {
-	return this.coap.send_message(path, coap_msg, send_cb);
-}
+	Coap.prototype.start_server = function (verify_psk_cb) {
+		return this.coap.start_server(verify_psk_cb);
+	}
 
-Coap.prototype.observe = function(path, msg_type, observe_cb, options, token) {
-	return this.coap.observe(path, msg_type, observe_cb, options, token);
-}
+	Coap.prototype.stop_server = function () {
+		return this.coap.stop_server();
+	}
 
-Coap.prototype.cancel_observe = function(path, token, send_cb) {
-	return this.coap.cancel_observe(path, token, send_cb);
-}
+	Coap.prototype.send_message = function (path, coap_msg, send_cb) {
+		return this.coap.send_message(path, coap_msg, send_cb);
+	}
 
-Coap.prototype.init_resources = function(resources) {
-	return this.coap.init_resources(resources);
-}
+	Coap.prototype.observe = function (path, msg_type, observe_cb, options, token) {
+		return this.coap.observe(path, msg_type, observe_cb, options, token);
+	}
 
-Coap.prototype.notify_resource_changed = function(path) {
-	return this.coap.notify_resource_changed(path);
-}
+	Coap.prototype.cancel_observe = function (path, token, send_cb) {
+		return this.coap.cancel_observe(path, token, send_cb);
+	}
 
-module.exports.CONTENT_FORMAT = {
-	'PLAIN': 	0,
-	'LINK_FORMAT': 	40,
-	'XML': 		41,
-	'OCTET_STREAM': 42,
-	'EXI': 		47,
-	'JSON': 	50,
-	'CBOR': 	60
-}
+	Coap.prototype.init_resources = function (resources) {
+		return this.coap.init_resources(resources);
+	}
 
-module.exports.OPTION_KEY = {
-	'IF_MATCH': 		1,
-	'URI_HOST': 		3,
-	'ETAG': 		4,
-	'IF_NONE_MATCH': 	5,
-	'OBSERVE': 		6,
-	'URI_PORT': 		7,
-	'LOCATION_PATH': 	8,
-	'URI_PATH': 		11,
-	'CONTENT_FORMAT': 	12,
-	'CONTENT_TYPE': 	12,
-	'MAXAGE': 		14,
-	'URI_QUERY': 		15,
-	'ACCEPT': 		17,
-	'LOCATION_QUERY': 	20,
-	'BLOCK2':		23,
-	'PROXY_URI': 		35,
-	'PROXY_SCHEME': 	39,
-	'SIZE1': 		60
-};
+	Coap.prototype.notify_resource_changed = function (path) {
+		return this.coap.notify_resource_changed(path);
+	}
 
-module.exports.CODE = {
-	'EMPTY': 			0,
-	'GET': 				1,
-	'POST': 			2,
-	'PUT': 				3,
-	'DELETE': 			4,
-	'CREATED': 			65,
-	'DELETED': 			66,
-	'VALID': 			67,
-	'CHANGED': 			68,
-	'CONTENT': 			69,
-	'BAD_REQUEST': 			128,
-	'UNAUTHORIZED': 		129,
-	'BAD_OPTION': 			130,
-	'FORBIDDEN': 			131,
-	'NOT_FOUND': 			132,
-	'METHOD_NOT_ALLOWED': 		133,
-	'NOT_ACCEPTABLE': 		134,
-	'PRECONDITION_FAILED': 		140,
-	'REQ_ENTITY_TOO_LARGE':  	141,
-	'UNSUPPORTED_CONTENT_FORMAT':  	143,
-	'INTERNAL_SERVER_ERROR': 	160,
-	'NOT_IMPLEMENTED': 		161,
-	'BAD_GATEWAY': 			162,
-	'SERVICE_UNAVAILABLE': 		163,
-	'GATEWAY_TIMEOUT': 		165,
-	'PROXY_NOT_SUPPORTED': 		166
-};
+	module.exports.CONTENT_FORMAT = {
+		'PLAIN': 0,
+		'LINK_FORMAT': 40,
+		'XML': 41,
+		'OCTET_STREAM': 42,
+		'EXI': 47,
+		'JSON': 50,
+		'CBOR': 60
+	}
 
-module.exports.RESOURCE_NOTIF_TYPE = {
-	'NON': 0x0,
-	'CON': 0x2
+	module.exports.OPTION_KEY = {
+		'IF_MATCH': 1,
+		'URI_HOST': 3,
+		'ETAG': 4,
+		'IF_NONE_MATCH': 5,
+		'OBSERVE': 6,
+		'URI_PORT': 7,
+		'LOCATION_PATH': 8,
+		'URI_PATH': 11,
+		'CONTENT_FORMAT': 12,
+		'CONTENT_TYPE': 12,
+		'MAXAGE': 14,
+		'URI_QUERY': 15,
+		'ACCEPT': 17,
+		'LOCATION_QUERY': 20,
+		'BLOCK2': 23,
+		'PROXY_URI': 35,
+		'PROXY_SCHEME': 39,
+		'SIZE1': 60
+	};
+
+	module.exports.CODE = {
+		'EMPTY': 0,
+		'GET': 1,
+		'POST': 2,
+		'PUT': 3,
+		'DELETE': 4,
+		'CREATED': 65,
+		'DELETED': 66,
+		'VALID': 67,
+		'CHANGED': 68,
+		'CONTENT': 69,
+		'BAD_REQUEST': 128,
+		'UNAUTHORIZED': 129,
+		'BAD_OPTION': 130,
+		'FORBIDDEN': 131,
+		'NOT_FOUND': 132,
+		'METHOD_NOT_ALLOWED': 133,
+		'NOT_ACCEPTABLE': 134,
+		'PRECONDITION_FAILED': 140,
+		'REQ_ENTITY_TOO_LARGE': 141,
+		'UNSUPPORTED_CONTENT_FORMAT': 143,
+		'INTERNAL_SERVER_ERROR': 160,
+		'NOT_IMPLEMENTED': 161,
+		'BAD_GATEWAY': 162,
+		'SERVICE_UNAVAILABLE': 163,
+		'GATEWAY_TIMEOUT': 165,
+		'PROXY_NOT_SUPPORTED': 166
+	};
+
+	module.exports.RESOURCE_NOTIF_TYPE = {
+		'NON': 0x0,
+		'CON': 0x2
+	}
 }
